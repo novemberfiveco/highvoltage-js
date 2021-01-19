c52ada2ed04d65601da98246ed4837fd92dea2ce
Jan Stevens <jan.stevens@novemberfive.co>
2021-01-19 14:10:28 +0100

    fix: typo in eslint-config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ["react-app", "plugin:prettier/recommended"],
   plugins: ['prettier', 'import'],
   rules: {
-    "react/jsx-key": ['error', { checkFragmentShorthand: true, checkKeyMustBeforeSpread: true },
+    "react/jsx-key": ['error', { checkFragmentShorthand: true, checkKeyMustBeforeSpread: true }],
     "@typescript-eslint/no-explicit-any": 'error',
     "import/newline-after-import": ['error', { "count": 1 }],
     "no-unused-vars": 'off',
